17d29328e4ccf2c18465d172d390d7637351207c
aadesh <aadeshkale0@gmail.com>
2019-04-21 15:26:49 +0530

    REST_post_request_with_JSONparse.parse

--- 1.serilizer_Api_GET_data/serilizer/urls.py
+++ 1.serilizer_Api_GET_data/serilizer/urls.py
@@ -4,10 +4,10 @@ from rest_framework import routers
 from myapp.views import EmpView,EmpViewAp
 
 router = routers.DefaultRouter()
-router.register(r'emp/',EmpView)
+router.register(r'emp',EmpView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
-    path('emp/',EmpViewAp.as_view()),
+    # path('emp/',EmpViewAp.as_view()),
 ]
